ded4bd620acd8d59f3cf796a1de259cdfb022b80
unknown <2612130022@ACADEMICO.IESB.BR>
2026-03-09 21:24:10 -0300

    fix: correção de nomenclatura de variável

--- Atividade_Busca/ultima-ocorrencia.c
+++ Atividade_Busca/ultima-ocorrencia.c
@@ -1,7 +1,7 @@
 int ultimaOcorrencia(int v[], int size, int val) {
     int indice = -1;
     for (int i = 0; i < size; i++) {
-        if (vetor[i] == valor) {
+        if (v[i] == val) {
             indice = i;
         }
     }
